9f11662f5eff572b2f6a580c7b6aad91feb9ccb7
J-Skarbek <john@snarkmediaagency.com>
2022-07-13 17:34:59 -0500

    Add html tables, get the field values to clear from inputs on button press. Work on test function to make sure looping through the created array works.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 const submitBtn = document.getElementById('submit-btn');
-let bookTitle = document.getElementById('title');
-let bookAuthor = document.getElementById('author');
-let bookPages = document.getElementById('pages');
-
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const bookPages = document.getElementById('pages');
+const testButton = document.getElementById('test-button');
 
 submitBtn.addEventListener('click', addBookToLibrary);
-// submitBtn.addEventListener('click', () => {
-//   console.log(newBook);
-// })
+testButton.addEventListener('click', displayBooks);
 
 const myLibrary = [];
 
@@ -24,4 +22,13 @@ function Book(title, author, pageCount, haveRead) {
 function addBookToLibrary() {
   let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, 'I have read');
   myLibrary.push(newBook);
-};
+  bookTitle.value = ''
+  bookAuthor.value = ''
+  bookPages.value = ''
+}
+
+function displayBooks() {
+  myLibrary.forEach(book => {
+    alert(this.title)
+  });
+}
