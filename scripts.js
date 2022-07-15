c05cd694f4fb9772ebcc2320f71eebb75c06ee12
J-Skarbek <john@snarkmediaagency.com>
2022-07-15 15:04:49 -0500

    Fixed issue with return statement on read/not read status. Add a couple of changes to HTML/CSS.

--- scripts.js
+++ scripts.js
@@ -5,54 +5,39 @@ const bookPages = document.getElementById('pages');
 const haveRead = document.getElementById('have-read');
 const testButton = document.getElementById('test-button');
 
-const displayTable = document.getElementById('books-table')
+const displayTable = document.getElementById('books-table');
 
 const myLibrary = [];
 
 submitBtn.addEventListener('click', addBookToLibrary);
 
-function Book(title, author, pageCount, haveRead) {
-  this.title = title
-  this.author = author
-  this.pageCount = pageCount
-  this.haveRead = () => {
-    if (haveRead.checked) {
-      return this.readStatus = `I've read ${this.title}.`
+function Book(title, author, pageCount, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.readStatus = readStatus;
+  this.readStatusOutput = function() {
+    if (this.readStatus === true) {
+      return `I have read ${this.title}.`
     } else {
-       return this.readStatus = `I haven't read ${this.title}.`
+      return `I have not read ${this.title}.`
     }
-  }
-  // this.displayInfo = () => {
-  //   return `${title} by ${author}, ${pageCount} pages. ${readStatus}`;
-  // }
+  };
 }
 
 function addBookToLibrary() {
-  let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, 'I have read');
-  myLibrary.push(newBook);
-  displayBook(newBook);
+  let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, haveRead.checked)
+  myLibrary.push(newBook)
+  displayBook(newBook)
   bookTitle.value = ''
   bookAuthor.value = ''
   bookPages.value = ''
+  haveRead.checked = false
 }
 
 function displayBook(newBook) {
-  const tableRow = document.createElement('tr');
-  const newRow = displayTable.appendChild(tableRow);
+  const tableRow = document.createElement('tr')
+  const newRow = displayTable.appendChild(tableRow)
   newRow.innerHTML = 
-    `<tr><td>${newBook.title}</td><td>${newBook.author}</td><td>${newBook.pageCount}</td><td>${newBook.readStatus}</td><td><button>Remove</button></td></tr>`;
+    `<tr><td>${newBook.title}</td><td>${newBook.author}</td><td>${newBook.pageCount}</td><td>${newBook.readStatusOutput()}</td><td><button>Remove</button></td></tr>`
 }
-
-
-
-
-
-// function displayBooks() {
-//   myLibrary.forEach(book => {
-//      const tableRow = document.createElement('tr');
-//     const newRow = displayTable.appendChild(tableRow);
-//     newRow.innerHTML = 
-//       `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.pageCount}</td><td>${book.haveRead}</td></tr>`;
-//     console.log(newRow);
-//   });
-// }
