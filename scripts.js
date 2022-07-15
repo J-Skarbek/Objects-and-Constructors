bc15370806f985aff8667b9ca28307b07c396302
J-Skarbek <john@snarkmediaagency.com>
2022-07-15 11:39:50 -0500

    Rework the function to add and display new books on submission.

--- scripts.js
+++ scripts.js
@@ -5,12 +5,12 @@ const bookPages = document.getElementById('pages');
 const testButton = document.getElementById('test-button');
 
 const displayTable = document.getElementById('books-table')
-const tableRow = document.createElement('tr');
+
+const myLibrary = [];
 
 submitBtn.addEventListener('click', addBookToLibrary);
 testButton.addEventListener('click', displayBooks);
 
-const myLibrary = [];
 
 function Book(title, author, pageCount, haveRead) {
   this.title = title
@@ -25,18 +25,29 @@ function Book(title, author, pageCount, haveRead) {
 function addBookToLibrary() {
   let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, 'I have read');
   myLibrary.push(newBook);
+  displayBook(newBook);
   bookTitle.value = ''
   bookAuthor.value = ''
   bookPages.value = ''
 }
 
-function displayBooks() {
-  myLibrary.forEach(book => {
-    alert(`${book.title}, ${book.author}, ${book.pageCount}, ${book.haveRead}`);
-    let newRow = displayTable.appendChild(tableRow).innerHTML = 
-      `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.pageCount}</td><td>${book.haveRead}</td></tr>`;
-    console.log(newRow);
-    return newRow;
-    // newRow.innerHTML
-  });
+function displayBook(newBook) {
+  const tableRow = document.createElement('tr');
+  const newRow = displayTable.appendChild(tableRow);
+  newRow.innerHTML = 
+    `<tr><td>${newBook.title}</td><td>${newBook.author}</td><td>${newBook.pageCount}</td><td>${newBook.haveRead}</td></tr>`;
 }
+
+
+
+
+
+// function displayBooks() {
+//   myLibrary.forEach(book => {
+//      const tableRow = document.createElement('tr');
+//     const newRow = displayTable.appendChild(tableRow);
+//     newRow.innerHTML = 
+//       `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.pageCount}</td><td>${book.haveRead}</td></tr>`;
+//     console.log(newRow);
+//   });
+// }
