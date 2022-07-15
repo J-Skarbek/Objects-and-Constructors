c806065fa9a7a9dcb0cbff5c71dac6b1da04e81a
J-Skarbek <john@snarkmediaagency.com>
2022-07-15 16:50:46 -0500

    Work on event listener and getting book objects to remove on click.

--- scripts.js
+++ scripts.js
@@ -11,24 +11,36 @@ const myLibrary = [];
 
 submitBtn.addEventListener('click', addBookToLibrary);
 
+function addRemoveBtnListener() {
+  const removeButtons = Array.from(document.getElementsByClassName('remove-btn'));
+  removeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      // displayTable.removeChild('tr')
+      console.log('test')
+    })
+  })
+}
+
 function Book(title, author, pageCount, readStatus) {
   this.title = title;
   this.author = author;
   this.pageCount = pageCount;
   this.readStatus = readStatus;
   this.readStatusOutput = function() {
-    if (this.readStatus === true) {
-      return `I have read ${this.title}.`
-    } else {
-      return `I have not read ${this.title}.`
-    }
+    return (this.readStatus === true ? `I have read ${this.title}.` : `I have not read ${this.title}.`)
   };
 }
+
+// function removeBookFromLibrary() {
+//   console.log('test')
+//   displayTable.removeChild()
+// }
 
 function addBookToLibrary() {
   let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, haveRead.checked)
   myLibrary.push(newBook)
   displayBook(newBook)
+  addRemoveBtnListener()
   bookTitle.value = ''
   bookAuthor.value = ''
   bookPages.value = ''
@@ -39,5 +51,5 @@ function displayBook(newBook) {
   const tableRow = document.createElement('tr')
   const newRow = displayTable.appendChild(tableRow)
   newRow.innerHTML = 
-    `<tr><td>${newBook.title}</td><td>${newBook.author}</td><td>${newBook.pageCount}</td><td>${newBook.readStatusOutput()}</td><td><button>Remove</button></td></tr>`
+    `<td>${newBook.title}</td><td>${newBook.author}</td><td>${newBook.pageCount}</td><td>${newBook.readStatusOutput()}</td><td><button class="remove-btn">Remove</button></td>`
 }
