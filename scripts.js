d29f433d93e01523df37bb4da5539bb6076292db
J-Skarbek <john@snarkmediaagency.com>
2022-08-08 17:13:18 -0500

    Add code to reset the dom-index values of displayed books. The allows the dom elements to match the index values of the books array.

--- scripts.js
+++ scripts.js
@@ -13,45 +13,51 @@ const myLibrary = [];
 
 submitBtn.addEventListener('click', addBookToLibrary);
 
-function Book(title, author, pageCount, readStatus) {
+function Book(title, author, pageCount, readStatus, indexNum) {
   this.title = title;
   this.author = author;
   this.pageCount = pageCount;
   this.readStatus = readStatus;
+  this.bookReferenceNumber = indexNum;
   this.readStatusOutput = function() {
     return (this.readStatus === true ? `I have read ${this.title}.` : `I have not read ${this.title}.`)
   };
 }
 
 function addBookToLibrary() {
-  const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, haveRead.checked)
+  const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, haveRead.checked, indexNum)
   myLibrary.push(newBook)
-  handleBookDisplay(newBook) 
+  handleBookDisplay(newBook)
+  let tempBookReference = bookTitle.value 
   bookTitle.value = ''
   bookAuthor.value = ''
   bookPages.value = ''
   haveRead.checked = false
+  console.log(tempBookReference)
 }
 
 function bookRemovalOperatives(newBook) {
   const removalBtn = document.querySelector(`.remove-btn-${indexNum}`)
-    const removeRow = document.getElementById(`index-${indexNum}`)
-    removalBtn.addEventListener('click', () => {
-      displayTable.removeChild(removeRow)
-      myLibrary.pop(newBook)
-    })
+  const removeRow = document.getElementById(`index-${indexNum}`)
+  removalBtn.addEventListener('click', () => {
+    displayTable.removeChild(removeRow)
+    resetDomIndexCounter()
+      let test = removeRow.dataset.index
+      myLibrary.splice(`${test}`, 1)
+      console.log(`This is the test value: ${test}.`)
+  })
 }
 
 function createNewTableRow(newBook) {
   const tableRow = document.createElement('tr')
     const newRow = displayTable.appendChild(tableRow)
     newRow.setAttribute('id', `index-${indexNum}`)
+    newRow.setAttribute('class', 'books-in-dom')
     newRow.innerHTML = 
       `<td>${newBook.title}</td><td>${newBook.author}</td><td>${newBook.pageCount}</td><td>${newBook.readStatusOutput()}</td><td><button class="remove-btn-${indexNum}">Remove</button></td>`;
     bookRemovalOperatives(newBook)
     newRow.dataset.index = indexNum++
 }
-
 
 function handleBookDisplay(newBook) {
   if (myLibrary.length === 1) {
@@ -59,8 +65,28 @@ function handleBookDisplay(newBook) {
       createNewTableRow(newBook)
     }
   } else {
-    for (let i = myLibrary.length; i == myLibrary.length; i++) {
+    for (let i = myLibrary.length; i === myLibrary.length; i++) {
       createNewTableRow(newBook)
     }
   }
+}
+
+// myLibarary.forEach(bookElement => {
+  
+// });
+
+// get the dom reference 
+
+// run function to test each item in array to see if dom-reference number matches the array index value
+
+// if true -> remove/slice/pop that item from the array
+
+// run a loop through existing dom customElements, and for each one then re-assign the indexNum value
+
+function resetDomIndexCounter() {
+  indexNum = 0;
+  let domElementsValues = Array.from(document.getElementsByClassName('books-in-dom'))
+    domElementsValues.forEach(domElement => {
+      domElement.dataset.index = indexNum++
+  })
 }
